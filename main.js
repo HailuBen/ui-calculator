502dd9ed8dd4a5630ba872f971b117d8aca4d232
HailuBen <b.hailu.pk@gmail.com>
2022-07-08 06:40:52 -0400

    connect buttons & display

--- main.js
+++ main.js
@@ -1,11 +1,49 @@
 function setUpEvents(){
     numbers = [9,0,2,1,0];
 
-    const add = numbers.reduce((accumulator, currentValue)=>{
-        return accumulator+currentValue;
-    },0)
-    
-    console.log(add);
+    // const add = numbers.reduce((accumulator, currentValue)=>{
+    //     return accumulator+currentValue;
+    // },0);
+
+    // const subtract = numbers.reduce((a, b) => {
+    //     return a-b;
+    // });
+
+    // const multiply = numbers.reduce((a, b) => {
+    //     return a*b;
+    // });
+
+    // const divide = numbers.reduce((a, b) => {
+    //     return a/b;
+    // });
+    // console.log(add, subtract, multiply, divide);
+
+    // NUMBERS
+    const btn1 = document.getElementById('1');
+    const btn2 = document.getElementById('2');
+    const btn3 = document.getElementById('3');
+    const btn4 = document.getElementById('4');
+    const btn5 = document.getElementById('5');
+    const btn6 = document.getElementById('6');
+    const btn7 = document.getElementById('7');
+    const btn8 = document.getElementById('8');
+    const btn9 = document.getElementById('9');
+    const btn0 = document.getElementById('0');
+
+    // OPERATORS
+    const addBtn = document.getElementById('+');
+    const subtractBtn = document.getElementById('-');
+    const multiplyBtn = document.getElementById('*');
+    const divideBtn = document.getElementById('/');
+
+    // DECIMAL, EQUALS
+    const decimalBtn = document.getElementById('decimal');
+    const equalsBtn = document.getElementById('equals');
+
+    // DISPLAY
+    const numberDisplay = document.querySelector('.display-text');
+    numberDisplay.innerHTML+="suka la mink";
+
 }   
 window.onload = function(){
     setUpEvents();
