abe06d9799105479ce6bea21ffc65d24853fb1ef
HailuBen <b.hailu.pk@gmail.com>
2024-06-28 18:23:12 -0400

    add temp x var for display number, add operate function

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ function setUpEvents(){
     // Variables
     let btnValue=0;
     let clicked = false;
+    let x,y;
     let numberArr = [];
 
     // NUMBERS
@@ -17,23 +18,21 @@ function setUpEvents(){
     const btn8 = document.getElementById('8');
     const btn9 = document.getElementById('9');
     const btn0 = document.getElementById('0');
-
     // OPERATORS
     const addBtn = document.getElementById('+');
     const subtractBtn = document.getElementById('-');
     const multiplyBtn = document.getElementById('*');
     const divideBtn = document.getElementById('/');
-
     // DECIMAL, EQUALS
     const decimalBtn = document.getElementById('decimal');
     const equalsBtn = document.getElementById('equals');
-
-    // DISPLAY
+    // DISPLAY TEXT/CHARACTERS/NUMBERS
     const calcDisplay = document.querySelector('.display-text');
-
-    //CLEAR 
+    //CLEAR & DELETE
     const clearBtn = document.getElementById('clear');
+    const deleteBtn = document.getElementById('delete');
     
+
     // EVENT LISTENER TO DISPLAY BUTTON PRESS
     document.querySelectorAll('button').forEach(ele => ele.addEventListener('click', function(){
         clicked = true;
@@ -46,13 +45,25 @@ function setUpEvents(){
 
     // EVENT LISTENER TO CLEAR DISPLAY SCREEN & ARRAY
     clearBtn.addEventListener("click", clearDisplay);
+    // EVENT LISTENER TO DELETE THE PREVIOUS CHARACTER FRON DISPLAY SCREEN & ARRAY
+    deleteBtn.addEventListener("click", deleteSpace);
 
     
-    // FUNCTION TO SEND NUMBER PRESSED TO ARRAY
+    // FUNCTION TO GET NUMBER ENTERED INTO DISPLAY & OPERATOR
     function getEquation(){
         if (btnValue!== '=' && btnValue!== '/' && btnValue!== '*' && btnValue!== '+' && btnValue!== '-' && btnValue!== 'clear' && btnValue!== 'delete'){
-            numberArr.push(btnValue);
-            console.log(numberArr);
+            x = parseFloat(calcDisplay.innerHTML);
+            console.log(typeof(x)+": "+x);
+        }
+    }
+
+    // WHEN AN OPERATION IS CLICKED, ADD THE CURRENT DISPLAY NUMBER TO ARRAY FOR OPERATION
+    function operate(){
+        if (btnValue!== '=' && btnValue!== '/' && btnValue!== '*' && btnValue!== '+' && btnValue!== '-' && btnValue!== 'clear' && btnValue!== 'delete'){
+            console.log(typeof(x)+"Current Number: "+x);
+            // numberArr.push(x);
+            // console.log(numberArr);
+            // ^^^ ADDING NUMBER TO ARRAY AFTER AN OPERATION IS PRESSED
         }
     }
 
@@ -61,6 +72,14 @@ function setUpEvents(){
         calcDisplay.innerHTML = "";
         numberArr = [];
     }
+
+    // DELETE SINGLE PREVIOUS SPACE (BACKSPACE) 
+    function deleteSpace(){
+        // value.substr(0, value.length - 1);
+    }
+
+
+
 
 }   
 window.onload = function(){
