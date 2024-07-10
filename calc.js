b72b14142096e95a3f7bd19fd83d3bedaed514bc
HailuBen <b.hailu.pk@gmail.com>
2024-07-10 19:11:15 -0400

    add counters, num limit, answerBox

--- calc.js
+++ calc.js
@@ -2,19 +2,26 @@ function setUpEvents() {
 
     // Global Variables
     let btnValue, operator;
-    let currentNumber, previousNumber, answer;
+    let currentNumber, previousNumber, answer, roundedAnswer;
+    let operatorCounter = 0;
+    let decimalCounter = 0;
 
     // Variable to get characters onto calculator screen
     const calcDisplay = document.querySelector('.display-text');
     // Variable to display an equals sign for calculations
-    const answerBox = document.querySelector('.answer-box');    
+    const answerBox = document.querySelector('.answer-box');
+    // Variable to disable decimal button
+    const decimalBtn = document.getElementById('decimal');
+
 
     // Event listener for button presses
     document.querySelectorAll('button').forEach(ele => ele.addEventListener('click', function () {
-        if (isNaN(btnValue) && btnValue !== '.'){
-          calcDisplay.innerHTML = '';  
+        console.log(calcDisplay.innerHTML.length);
+
+        if (isNaN(btnValue) && btnValue !== '.') {
+            calcDisplay.innerHTML = '';
         }
-        
+
         btnValue = this.value;
 
         switch (btnValue) {
@@ -25,40 +32,82 @@ function setUpEvents() {
                 deleteSpace();
                 break;
             case '/':
+                answerBox.innerHTML = '';
+                decimalCounter = 0;
+                decimalBtn.disabled = false;
                 operator = '/';
-                console.log("Operator: "+ operator);
-                updateNumber();
+                operatorCounter++;
+                console.log("Operator " + operatorCounter + ": " + operator);
+                if (operatorCounter <= 1) { //if this counter is greater than 1, it means multiple operations have been pressed consecutively and it will only calculate using the most recent operator selection.
+                    updateNumber();
+                }
+
                 calcDisplay.innerHTML = '÷'
                 break;
             case '*':
+                answerBox.innerHTML = '';
+                decimalCounter = 0;
+                decimalBtn.disabled = false;
                 operator = '*';
-                console.log("Operator: "+ operator);
-                updateNumber();
+                operatorCounter++;
+                console.log("Operator " + operatorCounter + ": " + operator);
+                if (operatorCounter <= 1) {
+                    updateNumber();
+                }
+
                 calcDisplay.innerHTML = 'x'
                 break;
             case '+':
+                answerBox.innerHTML = '';
+                decimalCounter = 0;
+                decimalBtn.disabled = false;
                 operator = '+';
-                console.log("Operator: "+ operator);
-                updateNumber();
+                operatorCounter++;
+                console.log("Operator " + operatorCounter + ": " + operator);
+                if (operatorCounter <= 1) {
+                    updateNumber();
+                }
+
                 calcDisplay.innerHTML = '+'
                 break;
             case '-':
+                answerBox.innerHTML = '';
+                decimalCounter = 0;
+                decimalBtn.disabled = false;
                 operator = '-';
-                console.log("Operator: "+ operator);
-                updateNumber();
+                operatorCounter++;
+                console.log("Operator " + operatorCounter + ": " + operator);
+                if (operatorCounter <= 1) {
+                    updateNumber();
+                }
+
                 calcDisplay.innerHTML = '-'
                 break;
             case '=':
                 equals();
+                decimalCounter = 0;
+                decimalBtn.disabled = false;
+                operatorCounter = 0; //reset
                 break;
             default:
                 if (!isNaN(btnValue) || btnValue == '.') { // check if btn is a number or decimal, if true, display btnValue and run getNumber
+                    if (btnValue == '.') {
+                        decimalCounter++;
+                        if (decimalCounter > 0) {
+                            decimalBtn.disabled = true;
+                        }
+                    }
                     let displayStr = calcDisplay.innerHTML;
 
                     // if display doesn't include an operator character, display number. else, clear then display number
                     if (!displayStr.includes('÷') && !displayStr.includes('x') && !displayStr.includes('-') && !displayStr.includes('+')) {
-                        calcDisplay.innerHTML += btnValue;
-                        getNumber();
+                        if (calcDisplay.innerHTML.length <= 14) {
+                            calcDisplay.innerHTML += btnValue;
+                            getNumber();
+                        }else{
+                            // do something to show user the max character limit
+                        }
+
                     } else {
                         clearDisplay();
                         calcDisplay.innerHTML += btnValue;
@@ -69,11 +118,12 @@ function setUpEvents() {
         }
     }))
 
+
     function getNumber() {
         currentNumber = parseFloat(calcDisplay.innerHTML);
         console.log('Current Number: ' + currentNumber);
     }
-    function updateNumber(){
+    function updateNumber() {
         previousNumber = parseFloat(currentNumber);
         currentNumber = 0;
     }
@@ -103,29 +153,33 @@ function setUpEvents() {
     // Operation Functions
     function divide() {
         answer = (previousNumber / currentNumber);
-        console.log("prev: "+previousNumber+"\n current: "+currentNumber);
-        answerBox.innerHTML += '=';
-        calcDisplay.innerHTML =  answer;
+        console.log("prev: " + previousNumber + "\n current: " + currentNumber);
+        answerBox.innerHTML = '=';
+        calcDisplay.innerHTML = answer;
     }
     function multiply() {
         answer = (previousNumber * currentNumber);
-        console.log("prev: "+previousNumber+"\n current: "+currentNumber);
-        answerBox.innerHTML += '=';
-        calcDisplay.innerHTML =  answer;
+        console.log("prev: " + previousNumber + "\n current: " + currentNumber);
+        answerBox.innerHTML = '=';
+        calcDisplay.innerHTML = answer;
     }
     function subtract() {
         answer = (previousNumber - currentNumber);
-        console.log("prev: "+previousNumber+"\n current: "+currentNumber);
-        answerBox.innerHTML += '=';
-        calcDisplay.innerHTML =  answer;
+        console.log("prev: " + previousNumber + "\n current: " + currentNumber);
+        answerBox.innerHTML = '=';
+        calcDisplay.innerHTML = answer;
     }
     function add() {
         answer = (previousNumber + currentNumber);
-        console.log("prev: "+previousNumber+"\n current: "+currentNumber);
-        answerBox.innerHTML += '=';
-        calcDisplay.innerHTML =  answer;
+        console.log("prev: " + previousNumber + "\n current: " + currentNumber);
+        answerBox.innerHTML = '=';
+        if (answer.toString().length >= 13) {
+            roundedAnswer = Math.round((answer + Number.EPSILON) * 100) / 100;
+            calcDisplay.innerHTML = roundedAnswer;
+        } else {
+            calcDisplay.innerHTML = answer;
+        }
     }
-
 
 
     // Clear calc screen 
@@ -134,6 +188,8 @@ function setUpEvents() {
         calcDisplay.innerHTML = '';
         currentNumber = 0;
         previousNumber = 0;
+        decimalCounter = 0;
+        decimalBtn.disabled = false;
     }
     // Delete previous space 
     function deleteSpace() {
